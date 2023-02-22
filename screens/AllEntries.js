d033d25a4dac9457436ff2b845fd4b029123a71d
Boriswaikin <plmnboris@gmail.com>
2023-02-22 01:11:47 -0800

    update realtime lister to the update of database, also import component EntriesList

--- screens/AllEntries.js
+++ screens/AllEntries.js
@@ -1,10 +1,38 @@
-import { View, StyleSheet,Button } from 'react-native'
-import React, {useEffect} from 'react'
+import { View, StyleSheet,Button ,FlatList,Text} from 'react-native'
+import React, {useState,useEffect} from 'react'
 import Color from '../components/Color'
+import EntriesList from '../components/EntriesList';
+import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { db } from '../Firebase/Firebase-setup';
+export default function AllEntries() {
 
-export default function AllEntries({ navigation }) {
+  const [entries,setEntries] = useState([]);
+
+  useEffect(()=>{
+    onSnapshot(collection(db, "entries"),(querySnapshot) => {
+      if (querySnapshot.empty){
+        setEntries([]);
+      }
+      else {
+        let entriesFromDB = [];
+        querySnapshot.docs.forEach((doc)=>{
+          entriesFromDB.push({...doc.data(),id:doc.id});
+        })
+        setEntries(entriesFromDB)
+        // console.log(entriesFromDB)
+      }
+    });
+    // return ()=>{
+    //   unsubscribe();
+    // }
+    },[])
+
+
+  // console.log(route.params.value);
   return (
     <View style={styles.container}>
+      <EntriesList
+        inputData={entries}/>
     </View>
   )
 }
