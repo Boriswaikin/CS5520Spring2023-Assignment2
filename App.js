1780aa51bc6d6cea62bc653538ed3f24656cf259
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:38:43 -0800

    update the format of the code

--- App.js
+++ App.js
@@ -1,34 +1,41 @@
-import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AddEntries from './screens/AddEntries';
-import EditEntries from './screens/EditEntries';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import Color from './components/Color';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AddEntries from "./screens/AddEntries";
+import EditEntries from "./screens/EditEntries";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import Color from "./components/Color";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{            
-            headerStyle:{
-                backgroundColor:Color.headerTabColor
-              },
-            headerTintColor: Color.headerTintColor,
-            headerTitleStyle:{
-            fontSize:18
-            },
-        }}>
-        <Stack.Screen 
-          name="Home"
-          component={BottomTabNavigator}  
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Color.headerTabColor,
+          },
+          headerTintColor: Color.headerTintColor,
+          headerTitleStyle: {
+            fontSize: 18,
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={BottomTabNavigator} />
+        <Stack.Screen
+          name="AddEntries"
+          component={AddEntries}
+          options={{
+            title: "Add An Entry",
+          }}
         />
-         <Stack.Screen name ="AddEntries" component={AddEntries}
-         options={{
-          title:"Add An Entry"}}/>
-         <Stack.Screen name ="EditEntries" component={EditEntries}
-         options={{
-          title:"Edit Entry"}}/>
-      </Stack.Navigator>                   
+        <Stack.Screen
+          name="EditEntries"
+          component={EditEntries}
+          options={{
+            title: "Edit Entry",
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
--- screens/OverLimitEntries.js
+++ screens/OverLimitEntries.js
@@ -1,51 +1,49 @@
-import { View, StyleSheet } from 'react-native'
-import React, {useState,useEffect} from 'react'
-import Color from '../components/Color'
-import EntriesList from '../components/EntriesList';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
-import { db } from '../Firebase/Firebase-setup';
+import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import Color from "../components/Color";
+import EntriesList from "../components/EntriesList";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../Firebase/Firebase-setup";
 
-export default function OverLimitEntries({navigation}) {
+export default function OverLimitEntries({ navigation }) {
+  const [entriesOverlimit, setEntriesOverlimit] = useState([]);
 
-  const [entriesOverlimit,setEntriesOverlimit] = useState([]);
-
-  function navigate(entries){
-    navigation.navigate('EditEntries',{entriesItem: entries});
+  function navigate(entries) {
+    navigation.navigate("EditEntries", { entriesItem: entries });
   }
 
-  const q = query(collection(db, "entries"), 
-  where("flagOverlimit", "==", true),
-  where("reviewedStatus", "==", false));
-  useEffect(()=>{
-    const unsubscribe = onSnapshot(q,(querySnapshot) => {
-      if (querySnapshot.empty){
+  const q = query(
+    collection(db, "entries"),
+    where("flagOverlimit", "==", true),
+    where("reviewedStatus", "==", false)
+  );
+  useEffect(() => {
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      if (querySnapshot.empty) {
         setEntriesOverlimit([]);
-      }
-      else {
+      } else {
         let entriesFromDB = [];
-        querySnapshot.docs.forEach((doc)=>{
-          entriesFromDB.push({...doc.data(),id:doc.id});
-        })
+        querySnapshot.docs.forEach((doc) => {
+          entriesFromDB.push({ ...doc.data(), id: doc.id });
+        });
         setEntriesOverlimit(entriesFromDB);
       }
     });
-    return ()=>{
+    return () => {
       unsubscribe();
-    }
-    },[])
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
-      <EntriesList
-        inputData={entriesOverlimit} 
-        EntriesPressed={navigate}/>
+      <EntriesList inputData={entriesOverlimit} EntriesPressed={navigate} />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Color.contentColor,
-      justifyContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: Color.contentColor,
+    justifyContent: "center",
+  },
+});
