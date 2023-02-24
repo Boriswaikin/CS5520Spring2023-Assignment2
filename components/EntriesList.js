6fa9102fd38ae854f8b9756c70e541a92d4b18c2
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:24:03 -0800

    update to include entriesPressed function

--- components/EntriesList.js
+++ components/EntriesList.js
@@ -1,28 +1,35 @@
-import { View, Text, FlatList,StyleSheet } from 'react-native'
-import Color from './Color'
-import React from 'react'
-import EntriesItem from './EntriesItem';
+import { View, FlatList, StyleSheet } from "react-native";
+import Color from "./Color";
+import React from "react";
+import EntriesItem from "./EntriesItem";
 
-export default function EntriesList({inputData}) {
-    // console.log(inputData);
-    return (
-        <View style={styles.bottomContaineer}>
-        <FlatList
-            contentContainerStyle = {styles.contentContainerStyle}
-            data = {inputData}
-            renderItem={({item})=>{
-              return <EntriesItem entries={item} /> 
-            }}/>
-        </View>
-    )
+export default function EntriesList({ inputData, EntriesPressed }) {
+  return (
+    <View style={styles.bottomContaineer}>
+      <FlatList
+        contentContainerStyle={styles.contentContainerStyle}
+        data={inputData}
+        renderItem={({ item }) => {
+          return (
+            <EntriesItem
+              entries={item}
+              editEntriesPressed={() => {
+                EntriesPressed(item);
+              }}
+            />
+          );
+        }}
+      />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    bottomContaineer : {
-        flex:4,
-        backgroundColor: Color.contentColor,
-      },
-    contentContainerStyle:{
-    alignItems:'center',
-    marginTop:25,
-  }
-})
+  bottomContaineer: {
+    flex: 4,
+    backgroundColor: Color.contentColor,
+  },
+  contentContainerStyle: {
+    alignItems: "center",
+    marginTop: 25,
+  },
+});
