bf96c6bbfafe02239ab878916f353647546ca887
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:25:53 -0800

    update pressable button for the pressable effect

--- components/PressableButton.js
+++ components/PressableButton.js
@@ -1,33 +1,37 @@
 //Type rnfe
-import { View, Text ,Pressable,StyleSheet} from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React from "react";
+import Color from "./Color";
 
-export default function PressableButton(
-    
-     {buttonPressed,customizedStyle,pressedStyle,children}
-    // props,
-){
-    return (
-        <Pressable 
-        style= {({pressed})=>{
-            return [
-            styles.pressableDefault,
-            customizedStyle,
-            pressed && styles.pressedStyle]}}
-            android_ripple={{Color:"red"}}
-            onPress={buttonPressed}>
-            {children}
-        </Pressable>
-    )
+export default function PressableButton({
+  buttonPressed,
+  customizedStyle,
+  children,
+}) {
+  return (
+    <Pressable
+      style={({ pressed }) => {
+        return [
+          styles.pressableDefault,
+          customizedStyle,
+          pressed && styles.pressedStyle,
+        ];
+      }}
+      android_ripple={{ Color: Color.rippleEffect }}
+      onPress={buttonPressed}
+    >
+      {children}
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    pressableDefault: {
-        borderRadius:4,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    pressedStyle:{
-        backgroundColor:'rgba(210, 230, 255, 0.1)'
-    }
-})
+  pressableDefault: {
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressedStyle: {
+    backgroundColor: Color.buttonEffect,
+  },
+});
