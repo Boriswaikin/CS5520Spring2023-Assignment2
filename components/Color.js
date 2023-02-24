12de9b894e7065ddc06566ea0118f6be64c201ff
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:13:18 -0800

    update the color category for reuse

--- components/Color.js
+++ components/Color.js
@@ -2,13 +2,13 @@
  * //This is a helper file used to expose common color value
  */
 const Color = {
-    contentColor: "mediumpurple",
-    headerTabColor: "blueviolet",
-    headerTintColor: "white",
-    tabIconColor: "gold",
-    // cardComponentColor: "lightgray",
-    textColor: "darkslateblue", //Text color is rebeccapurple
-    buttonColor:"blueviolet",
-  };
-  export default Color;
-  
+  contentColor: "#ADA2FF",
+  headerTabColor: "#645CBB",
+  headerTintColor: "white",
+  tabIconColor: "gold",
+  textCalories: "black",
+  transparent: "#B1AFFF",
+  rippleEffect: "red",
+  buttonEffect: "rgba(210, 230, 255, 0.1)",
+};
+export default Color;
