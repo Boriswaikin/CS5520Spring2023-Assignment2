559bd51c518b52d6d2424a2cf95c5bfd6958f3e1
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:32:28 -0800

    update add screen on the state variable of calories limit and more parameter on input component

--- screens/AddEntries.js
+++ screens/AddEntries.js
@@ -1,42 +1,38 @@
-import { View, StyleSheet,Button ,Text,Alert} from 'react-native'
-import React, {useEffect, useState} from 'react'
-import Color from '../components/Color'
-import InputComponent from '../components/InputComponent';
-import PressableButton from '../components/PressableButton';
-import { writeToDB } from '../Firebase/Firebase-helper';
-export default function AddEntries({navigation}) {
-
+import { View, StyleSheet, Text, Alert } from "react-native";
+import React, { useState } from "react";
+import Color from "../components/Color";
+import InputComponent from "../components/InputComponent";
+import PressableButton from "../components/PressableButton";
+import { writeToDB } from "../Firebase/Firebase-helper";
+export default function AddEntries({ navigation }) {
   const [inputCalories, setCalories] = useState();
-  const [inputDescription, setDescription] = useState();
+  const [inputDescription, setDescription] = useState("");
   const [overlimit, setOverlimit] = useState(false);
-  
-  function setInputCalories(text){
-    setCalories(text);
-  }
-  
-  function navigate(){
-      navigation.navigate('Home',{screen:'All Entries'})
+  const reviewed = false;
+  const [caloriesLimit, setCaloriesLimit] = useState(500);
+
+  function navigate() {
+    navigation.navigate("Home", { screen: "All Entries" });
   }
 
-  function OnTextEnterDB(){
+  function OnTextEnterDB() {
     const newEntries = {
-      calories:inputCalories,
-      description:inputDescription,
-      flagOverlimit:overlimit
-      };
+      calories: inputCalories,
+      description: inputDescription,
+      flagOverlimit: overlimit,
+      reviewedStatus: reviewed,
+    };
     writeToDB(newEntries);
   }
 
-  function onEntriesPressed(){
-    if(isNaN(inputCalories)||inputCalories<0||inputCalories.length === 0){
-      Alert.alert("Invalid input","Please check your input values");
-    }
-    else {
+  function onEntriesPressed() {
+    if (isNaN(inputCalories) || inputCalories < 0 || !inputDescription.trim()) {
+      Alert.alert("Invalid input", "Please check your input values");
+    } else {
       OnTextEnterDB();
       navigate();
     }
   }
-
 
   function resetInput() {
     setCalories("");
@@ -44,107 +40,120 @@ export default function AddEntries({navigation}) {
   }
   return (
     <View style={styles.container}>
-      <View style={styles.content}/>
+      <View style={styles.content} />
       <View style={styles.firstInput}>
         <Text
-            style={{
-              fontSize: 20,
-              color: Color.textColor,
-              marginLeft: 10,
-              marginTop: 10,
-            }}>
-            Calories
+          style={{
+            fontSize: 20,
+            color: Color.headerTabColor,
+            marginLeft: 10,
+            marginTop: 10,
+          }}
+        >
+          Calories
         </Text>
         <InputComponent
-          inputHeight = {30}
-          inputChangeText={(text)=>{
-            setInputCalories(text);
-            setOverlimit(parseFloat(text)>500);
-            }}
+          inputHeight={30}
+          inputChangeText={(text) => {
+            setCalories(text);
+            setOverlimit(parseFloat(text) > caloriesLimit);
+          }}
           inputValue={inputCalories}
           inputAlign="left"
+          backgroundColor={Color.transparent}
+          width={210}
+          padding={5}
+          radius={6}
+          marginTop={10}
         />
       </View>
       <View style={styles.secondInput}>
-      <Text
-            style={{
-              fontSize: 20,
-              color: Color.textColor,
-              marginLeft: 10,
-              marginTop: 10,
-            }}>
-            Description
+        <Text
+          style={{
+            fontSize: 20,
+            color: Color.headerTabColor,
+            marginLeft: 10,
+            marginTop: 10,
+          }}
+        >
+          Description
         </Text>
         <InputComponent
-          inputHeight = {100}
+          inputHeight={100}
           inputChangeText={setDescription}
           inputValue={inputDescription}
           inputAlign="left"
+          backgroundColor={Color.transparent}
+          width={210}
+          padding={5}
+          radius={6}
+          marginTop={10}
         />
-      </View> 
+      </View>
       <View style={styles.fixToText}>
-          <PressableButton
-            customizedStyle = {{
-              backgroundColor: Color.buttonColor,
-              height:35,
-              width:100}}
-            buttonPressed= {()=>{
-              resetInput();
-            }}
-          >
-          <Text style={{color:'white'}}>Reset</Text>
-          </PressableButton>
-          <PressableButton
-            customizedStyle = {{
-              backgroundColor: Color.buttonColor,
-              height:35,
-              width:100}}
-            buttonPressed= {()=>{
-              onEntriesPressed();
-              }}
-          >
-          <Text style={{color:'white'}}>Submit</Text>
-          </PressableButton>
-        </View>
-      <View style={styles.emptyContent}/>
+        <PressableButton
+          customizedStyle={{
+            backgroundColor: Color.headerTabColor,
+            height: 35,
+            width: 100,
+          }}
+          buttonPressed={() => {
+            resetInput();
+          }}
+        >
+          <Text style={{ color: "white" }}>Reset</Text>
+        </PressableButton>
+        <PressableButton
+          customizedStyle={{
+            backgroundColor: Color.headerTabColor,
+            height: 35,
+            width: 100,
+          }}
+          buttonPressed={() => {
+            onEntriesPressed();
+          }}
+        >
+          <Text style={{ color: "white" }}>Submit</Text>
+        </PressableButton>
+      </View>
+      <View style={styles.emptyContent} />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection:'column',
-      backgroundColor: 'mediumpurple',
-      alignItems: 'stretch',
-      justifyContent: 'center',
-    },
-    content: {
-      flex: 1,
-    },
-    firstInput: {
-      marginLeft: 5,
-      marginRight: 30,
-      justifyContent: "space-between",
-      flex: 1,
-      backgroundColor: 'mediumpurple',
-      flexDirection: 'row',
-    },
-    secondInput: {
-      marginLeft: 5,
-      marginRight:30,
-      justifyContent: "space-between",
-      flex: 3,
-      backgroundColor: 'mediumpurple',
-      flexDirection: 'row',
-    },
-    fixToText: {
-      flex: 1,
-      marginLeft: 80,
-      marginRight: 80,
-      justifyContent: "space-between",
-      flexDirection: "row",
-    },
-    emptyContent: {
-      flex: 7,
-    },
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: Color.contentColor,
+    justifyContent: "center",
+  },
+  content: {
+    flex: 1,
+  },
+  firstInput: {
+    flex: 1,
+    marginLeft: 5,
+    marginRight: 30,
+    justifyContent: "space-between",
+    backgroundColor: Color.contentColor,
+    flexDirection: "row",
+  },
+  secondInput: {
+    flex: 3,
+    marginLeft: 5,
+    marginRight: 30,
+    justifyContent: "space-between",
+    backgroundColor: Color.contentColor,
+    flexDirection: "row",
+  },
+  fixToText: {
+    flex: 1,
+    marginLeft: 80,
+    marginRight: 80,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  emptyContent: {
+    flex: 7,
+  },
+});
