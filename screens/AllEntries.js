76ccc339e6f5104d278caeaf7738fb3bffc04df6
Boriswaikin <plmnboris@gmail.com>
2023-02-23 21:33:55 -0800

    update to set unsubscribe variable and set function to navigate to edit entries screen and pass it to component entriesItem

--- screens/AllEntries.js
+++ screens/AllEntries.js
@@ -1,45 +1,47 @@
-import { View, StyleSheet,Button ,FlatList,Text} from 'react-native'
-import React, {useState,useEffect} from 'react'
-import Color from '../components/Color'
-import EntriesList from '../components/EntriesList';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
-import { db } from '../Firebase/Firebase-setup';
-export default function AllEntries() {
+import { View, StyleSheet, Button, FlatList, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import Color from "../components/Color";
+import EntriesList from "../components/EntriesList";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../Firebase/Firebase-setup";
 
-  const [entriesAll,setEntriesAll] = useState([]);
+export default function AllEntries({ navigation }) {
+  const [entriesAll, setEntriesAll] = useState([]);
 
-  useEffect(()=>{
-    onSnapshot(collection(db, "entries"),(querySnapshot) => {
-      if (querySnapshot.empty){
-        setEntriesAll([]);
+  function navigate(entries) {
+    navigation.navigate("EditEntries", { entriesItem: entries });
+  }
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "entries"),
+      (querySnapshot) => {
+        if (querySnapshot.empty) {
+          setEntriesAll([]);
+        } else {
+          let entriesFromDB = [];
+          querySnapshot.docs.forEach((doc) => {
+            entriesFromDB.push({ ...doc.data(), id: doc.id });
+          });
+          setEntriesAll(entriesFromDB);
+        }
       }
-      else {
-        let entriesFromDB = [];
-        querySnapshot.docs.forEach((doc)=>{
-          entriesFromDB.push({...doc.data(),id:doc.id});
-        })
-        setEntriesAll(entriesFromDB)
-        // console.log(entriesFromDB)
-      }
-    });
-    return ()=>{
+    );
+    return () => {
       unsubscribe();
-    }
-    },[])
+    };
+  }, []);
 
-  // console.log(route.params.value);
   return (
     <View style={styles.container}>
-      <EntriesList
-        inputData={entriesAll}/>
+      <EntriesList inputData={entriesAll} EntriesPressed={navigate} />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Color.contentColor,
-      alignItems: 'stretch',
-      justifyContent: 'center',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: Color.contentColor,
+    justifyContent: "center",
+  },
+});
